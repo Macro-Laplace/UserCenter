af6afee31544b554e8e9350f661d7077d7468425
Macro-Laplace <810282264@qq.com>
2022-12-08 20:47:20 +0800

    前端优化

--- myapp/src/components/Footer/index.tsx
+++ myapp/src/components/Footer/index.tsx
@@ -1,22 +1,22 @@
 import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 const Footer: React.FC = () => {
-  const defaultMessage = '蚂蚁集团体验技术部出品';
+  const defaultMessage = 'Modify by Macro-Laplace';
   const currentYear = new Date().getFullYear();
   return (
     <DefaultFooter
       copyright={`${currentYear} ${defaultMessage}`}
       links={[
         {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
+          key: '给Macro-Laplace发邮件',
+          title: 'Mail',
+          href: 'mailto:Macro-Laplace@outlook.com',
           blankTarget: true,
         },
         {
-          key: 'github',
+          key: '源码地址',
           title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
+          href: 'https://github.com/Macro-Laplace/UserCenter',
           blankTarget: true,
         },
         {
---
+++ myapp/src/constants/index.ts
@@ -0,0 +1,1 @@
+export const SYSTEM_LOGO = "https://pic.imgdb.cn/item/63903183b1fccdcd36ee911e.jpg";
